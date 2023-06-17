6e0ea05e27f285c68197a806c3219610ea7dfa8d
ryansba <ryan@servicebureauccelerator.com>
2023-06-16 18:24:55 -0700

    modified:   gpt.py

--- gpt.py
+++ gpt.py
@@ -2,36 +2,28 @@ import pandas as pd
 
 csv_file = '/623payouts/olt_ra.csv'
 
-# CSV File Copied From Notepad++ 
-# Master,Office,Tech,Out
-# 007021,613605, $25.00 , $15.00 
-# 007021,563317, $25.00 , $15.00 
-# 007021,613605, $25.00 , $15.00 
-# 007021,613605, $25.00 , $15.00 
-# 007021,563317, $25.00 , $15.00 
-# FIG 1
 
 df = pd.read_csv(csv_file)
 
 print(df)
-#    Master  Office      Tech       Out
-# 0    7021  613605   $25.00    $15.00 
-# 1    7021  563317   $25.00    $15.00 
-# 2    7021  613605   $25.00    $15.00 
-# 3    7021  613605   $25.00    $15.00 
-# 4    7021  563317   $25.00    $15.00 
+
 
 print(df.dtypes)
-# Master     int64
-# Office     int64
-# Tech      object
-# Out       object
+
+def process_df(master):
+
+    filtered_df = df[df['Master'] == int(master)]
+
+    filtered_df['Tech'] = filtered_df['Tech'].replace({'\$': ''}, regex=True).astype(float)
+    filtered_df['Out'] = filtered_df['Out'].replace({'\$': ''}, regex=True).astype(float)
+    print(filtered_df)
+
+    result = filtered_df.groupby('Office').agg({'Tech': 'sum', 'Out': 'sum'}).reset_index()
 
 
-# Hi ChatGPT Please see instructions below
+    result['Office'] = result['Office'].apply(lambda x: str(x).zfill(6))
 
-# Write a function that accepts Master as arg and...
-# Returns back one row per unique value in the office col also...
-# Sums the values in Tech / Out and pretty please
-# I need to retain the format of Master and Office as they were in # FIG 1
+    return result
 
+result = process_df('007021')
+print(result)
