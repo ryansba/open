fc7352bb4ed1e3e88a1082f3f5c7a6c114fb2a53
ryansba <ryan@servicebureauccelerator.com>
2023-06-20 06:55:35 -0700

    modified:   gpt.py

--- gpt.py
+++ gpt.py
@@ -1,29 +1,8 @@
-import pandas as pd
-
-csv_file = '/623payouts/olt_ra.csv'
-
-
-df = pd.read_csv(csv_file)
-
-print(df)
-
-
-print(df.dtypes)
-
-def process_df(master):
-
-    filtered_df = df[df['Master'] == int(master)]
-
-    filtered_df['Tech'] = filtered_df['Tech'].replace({'\$': ''}, regex=True).astype(float)
-    filtered_df['Out'] = filtered_df['Out'].replace({'\$': ''}, regex=True).astype(float)
-    print(filtered_df)
-
-    result = filtered_df.groupby('Office').agg({'Tech': 'sum', 'Out': 'sum'}).reset_index()
-
-
-    result['Office'] = result['Office'].apply(lambda x: str(x).zfill(6))
-
-    return result
-
-result = process_df('007021')
-print(result)
+#### Hi GPT 4 
+#### Write python code with a simple setup/installation type GUI
+#### Not actually installing anything just setting up options for a script
+#### Options to setup: 
+# browse/import CSV files
+# 1). TaxSlayer  EFIN Volume CSV
+#2.) OLT EFIN Volume CSV
+#3.) Xlink EFIN volume CSV
